29a1924b7acec72e7011d236ba24bb3a0e3db99f
Patrick Krämer <patrickkraemerhn@aol.com>
2016-10-05 00:15:32 +0200

    demo program

--- src/MagicHomeController.Demo/Program.cs
+++ src/MagicHomeController.Demo/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 
 namespace MagicHomeController.Demo
 {
@@ -6,12 +9,23 @@ namespace MagicHomeController.Demo
 	{
 		static void Main(string[] args)
 		{
-			var allControllers = DeviceFinder.FindDevices().Select(x => new Device(x.IpAddress, DeviceType.RgbWarmwhite));
 
-			foreach (var controller in allControllers)
+			// find single device by mac address
+			var macAddr = PhysicalAddress.Parse("AA-11-22-33-44-55");
+			Device deviceByMac = Device.GetByMacAddress(macAddr, DeviceType.Rgb);
+
+			// use device by ip address
+			var ip = IPAddress.Parse("192.168.0.123");
+			Device deviceByIp = new Device(ip, DeviceType.Rgb); 
+
+			// search for devices in the network and use them
+			IEnumerable<DeviceFindResult> findResults = DeviceFinder.FindDevices(); // find ip, mac and model of all devices in network
+			IEnumerable<Device> allDevices = findResults.Select(result => new Device(result.IpAddress, DeviceType.Rgb)); // make new rgb device instance for every found device
+
+			foreach (var device in allDevices)
 			{
-				controller.TurnOn();
-				controller.SetColor(255, 0, 255);
+				device.TurnOn(); //turn on
+				device.SetColor(255, 0, 255); //set color to magenta
 			}
 		}
 	}
